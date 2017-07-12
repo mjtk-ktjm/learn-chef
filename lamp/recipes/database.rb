9cb9cbd3bfa0fa97a10eccb4e038014653b63f32
mjtk-ktjm <mjtk.ktjm@gmail.com>
2017-07-12 10:07:04 -0400

    still testing

--- lamp/recipes/database.rb
+++ lamp/recipes/database.rb
@@ -6,8 +6,7 @@
 
 # Load MySQL passwords from the 'passwords' data bag.
 # # passwords = data_bag_item('passwords', 'mysql')
-passwords['root_password'] = "#r00t"
-passwords['admin_password'] = "Adm1nPassw0rd"
+passwords = {root_password: "#r00t", admin_password: "Adm1n"}
 # Configure the MySQL client.
 mysql_client 'default' do
   action :create
