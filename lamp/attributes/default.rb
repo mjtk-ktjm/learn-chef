52d5aa222a1d2afc477e2ce0e2e6b205fb314203
mjtk-ktjm <mjtk.ktjm@gmail.com>
2017-07-12 17:56:11 -0400

    testing

--- lamp/attributes/default.rb
+++ lamp/attributes/default.rb
@@ -2,5 +2,5 @@ default['lamp']['web']['document_root'] = '/var/www/default/public_html'
 
 default['lamp']['database']['dbname'] = 'my_company'
 default['lamp']['database']['admin_username'] = 'db_admin'
-default['lamp']['database']['passwords_root_password'] = 't3st1ng'
+default['lamp']['database']['passwords_root_password'] = 'test1ng'
 default['lamp']['database']['passwords_admin_password'] = 'Adm1n$'
