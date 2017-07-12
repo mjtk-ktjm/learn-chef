e76e85dd93958726fc6f7043fa1970b21b68e399
mjtk-ktjm <mjtk.ktjm@gmail.com>
2017-07-12 10:10:52 -0400

    still testing

--- lamp/recipes/database.rb
+++ lamp/recipes/database.rb
@@ -30,16 +30,16 @@ mysql_connection_info = {
 }
 
 # Create the database instance.
-mysql_database node['lamp']['database']['dbname'] do
-  connection mysql_connection_info
-  action :create
-end
+#mysql_database node['lamp']['database']['dbname'] do
+#  connection mysql_connection_info
+#  action :create
+#end
 
 # Add a database user.
-mysql_database_user node['lamp']['database']['admin_username'] do
-  connection mysql_connection_info
-  password passwords['admin_password']
-  database_name node['lamp']['database']['dbname']
-  host '127.0.0.1'
-  action [:create, :grant]
-end
+#mysql_database_user node['lamp']['database']['admin_username'] do
+#  connection mysql_connection_info
+#  password passwords['admin_password']
+#  database_name node['lamp']['database']['dbname']
+#  host '127.0.0.1'
+#  action [:create, :grant]
+#end
